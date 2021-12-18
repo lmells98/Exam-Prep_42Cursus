f6f324fccf1ab9517ba68e419dda3b6b42095da3
lmells98 <Leighton/mells23@gmail.com>
2021-12-18 17:39:51 +0930

    Exam Preparation for Rank02

---
+++ Rank02/inter/inter.c
@@ -0,0 +1,45 @@
+#include <unistd.h>
+
+static short	check_first(char c, char *str, int pos)
+{
+	int	i;
+
+	i = 0;
+	while (i < pos)
+	{
+		if (str[i] == c)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	main(int ac, char **av)
+{
+	int	i;
+	int	j;
+
+	if (ac == 3)
+	{
+		i = 0;
+		while (av[1][i] != '\0')
+		{
+			j = 0;
+			while (av[2][j] != '\0')
+			{
+				if (av[1][i] == av[2][j])
+				{
+					if (check_first(av[1][i], av[1], i))
+					{
+						write(1, &av[1][i], 1);
+						break ;
+					}
+				}
+				j++;
+			}
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
--- Rank02/union/union.c
+++ Rank02/union/union.c
@@ -1,9 +1,42 @@
+#include <unistd.h>
 
+int	main(int ac, char **av)
+{
 	//Declare 2 index trackers and a fixed array with size of unsigned char.
+	int	i;
+	int	j;
+	int	used[255];
 	//This array will be our checking mechanism.
 	//Unsigned char has range of 0-255.
-	//Initialise array and fill with 0's.
-	//Assign first index value of 1. This is to index the arguments.
-	//Loop through argument characters 1 by 1.
-	//If We havent stored a value in the array at the index position given by the value of the unsigned char from the argument.
-	//We assign the array position a value of 1 and output the current character.
+	if (ac == 3)
+	{
+		//Initialise array and fill with 0's.
+		i = 0;
+		while (i < 255)
+		{
+			used[i] = 0;
+			i++;
+		}
+		//Assign first index value of 1. This is to index the arguments.
+		i = 1;
+		while (i < 3)
+		{
+			j = 0;
+			//Loop through argument characters 1 by 1.
+			while (av[i][j] != '\0')
+			{
+				//If We havent stored a value in the array at the index position given by the value of the unsigned char from the argument.
+				if (!used[(unsigned char)av[i][j]])
+				{
+					//We assign the array position a value of 1 and output the current character.
+					used[(unsigned char)av[i][j]] = 1;
+					write(1, &av[i][j], 1);
+				}
+				j++;
+			}
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
